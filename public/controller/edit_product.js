8359240448047314ba24a479fa021076057ba9e4
Terence Ateya <ateyaterence@gmail.com>
2021-06-17 23:14:12 +0100

    Code from vids 7 - 14
    Pending: Completion of vid 14
    See: todo on lin3 47, edit_product.js

--- public/controller/edit_product.js
+++ public/controller/edit_product.js
@@ -3,6 +3,52 @@ import * as FirebaseController from "./firebase_controller.js";
 import * as Util from "../viewpage/util.js";
 import * as Constant from "../model/constant.js";
 import * as Element from "../viewpage/element.js";
+import {
+    Product
+} from "../model/product.js";
+
+let imageFile2Upload;
+
+export function addEventListeners() {
+    Element.formEditProduct.imageButton.addEventListener('change', e => {
+        imageFile2Upload = e.target.files[0];
+        if (!imageFile2Upload) {
+            Element.formEditProduct.imageTag.src = null;
+            Element.formEditProduct.errorImage.innerHTML = 'Image change cancelled. The original image will be used';
+            return;
+        }
+        Element.formEditProduct.errorImage.innerHTML = '';
+        const reader = new FileReader();
+        reader.readAsDataURL(imageFile2Upload);
+        reader.onload = () => Element.formEditProduct.imageTag.src = reader.result;
+    });
+
+    Element.formEditProduct.form.addEventListener('submit', e => {
+        e.preventDefault();
+        const button = e.target.getElementsByTagName('button')[0];
+        const label = Util.disableButton(button);
+
+        const p = new Product({
+            name: e.target.name.value,
+            price: e.target.price.value,
+            summary: e.target.summary.value,
+            docId: e.target.docId.value
+        });
+        const errors = p.validate(true); // bypass image file check
+        Element.formEditProduct.errorName.innerHTML = errors.name ? errors.name : '';
+        Element.formEditProduct.errorPrice.innerHTML = errors.price ? errors.price : '';
+        Element.formEditProduct.errorSummary.innerHTML = errors.summary ? errors.summary : '';
+
+        if (Object.keys(errors).length != 0) {
+            Util.enableButton(button, label);
+            return;
+        }
+
+        // TODO: Complete vid 14 at 1:14s timestamp
+
+        Util.enableButton(button, label);
+    });
+}
 
 export async function edit_product(docId) {
     let product;
@@ -26,6 +72,7 @@ export async function edit_product(docId) {
     Element.formEditProduct.form.price.value = product.price;
     Element.formEditProduct.form.summary.value = product.summary;
     Element.formEditProduct.imageTag.src = product.imageURL;
+    Element.formEditProduct.errorImage.innerHTML = '';
 
     Element.modalEditProduct.show();
 }
--- public/model/product.js
+++ public/model/product.js
@@ -1,13 +1,13 @@
 export class Product {
-    constructor(data){
-        this.name = data.name.toLowerCase();
-        this.price = typeof data.price =='number' ? data.price : Number(data.price);
-        this.summary = data.summary;
+    constructor(data) {
+        this.name = data.name.toLowerCase().trim();
+        this.price = typeof data.price == 'number' ? data.price : Number(data.price);
+        this.summary = data.summary.trim();
         this.imageName = data.imageName;
         this.imageURL = data.imageURL;
     }
 
-    serialize(){
+    serialize() {
         return {
             name: this.name,
             price: this.price,
@@ -18,18 +18,18 @@ export class Product {
         }
     }
 
-    validate(imageFile){
+    validate(imageFile) {
         const errors = {};
         if (!this.name || this.name.length < 2)
             errors.name = 'Product name too short; min 3 chars expected'
-        if(!this.price || !Number(this.price))
+        if (!this.price || !Number(this.price))
             errors.price = 'Price is not a number';
         if (!this.summary || this.summary.length < 5)
             errors.summary = 'Product summary too short; min 5 chars'
         if (!imageFile)
             errors.image = 'Image not selected'
-            
+
         return errors;
-        
+
     }
 }
